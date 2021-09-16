40f655629df15dead662c44fdefb7b70f275e011
beacalvo <beacalvo96@gmail.com>
2021-09-16 12:15:19 +0200

    Moved .pprintnames function to utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,12 @@
+## pretty-print names (private)
+.pprintnames <- function(x) {
+    y <- x
+    if (length(x) > 2)
+        y <- c(y[1], "...", y[length(y)])
+    y <- paste(y, collapse=", ")
+    y
+}
+
 ## private function .checkPhenoData()
 
 #' @importFrom S4Vectors nrow rownames
